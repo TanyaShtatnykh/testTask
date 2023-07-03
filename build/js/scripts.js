ec90d386ca3e9ed2be6344b3054c1892a74b7da7
TanyaShtatnykh <tanya.shtatnykh@gmail.com>
2023-07-03 20:30:57 +0700

    Fixed

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(window).on('load resize', function() {
   if ($(window).width() < 576) {
     $('.footer__info--menu').insertAfter('.footer__logo-link');
@@ -11,19 +13,18 @@ $(function () {
     $('.header__logo-link').toggleClass('header__logo-link--hidden');
     $('.header__menu').toggleClass('header__menu--open');
     $('.header__socials').toggleClass('header__socials--open');
-
+  
+    if ($(this).hasClass('header__burger-btn--open')) {
+      $('.header__menu-link').on('click', function () {
+        $('.header__burger-btn').toggleClass('header__burger-btn--open');
+        $('.header__area').toggleClass('header__area--open');
+        $('.header__logo-link').toggleClass('header__logo-link--hidden');
+        $('.header__menu').toggleClass('header__menu--open');
+        $('.header__socials').toggleClass('header__socials--open');
+      })
+    };
   });
-})
-
-// if ($('.request--header').hasClass('request--header--active')) {
-//       $('.request--header').removeClass('request--header--active');
-//       $('.header__logo-link').removeClass('header__logo-link--hidden');
-// }
-// $(this).toggleClass('header__burger-btn--open');
-// $('.header__info').toggleClass('header__info--open');
-
-
-
+});
 
 
 $(function () {
--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -5,13 +5,15 @@ $(function () {
     $('.header__logo-link').toggleClass('header__logo-link--hidden');
     $('.header__menu').toggleClass('header__menu--open');
     $('.header__socials').toggleClass('header__socials--open');
-
+  
+    if ($(this).hasClass('header__burger-btn--open')) {
+      $('.header__menu-link').on('click', function () {
+        $('.header__burger-btn').toggleClass('header__burger-btn--open');
+        $('.header__area').toggleClass('header__area--open');
+        $('.header__logo-link').toggleClass('header__logo-link--hidden');
+        $('.header__menu').toggleClass('header__menu--open');
+        $('.header__socials').toggleClass('header__socials--open');
+      })
+    };
   });
-})
-
-// if ($('.request--header').hasClass('request--header--active')) {
-//       $('.request--header').removeClass('request--header--active');
-//       $('.header__logo-link').removeClass('header__logo-link--hidden');
-// }
-// $(this).toggleClass('header__burger-btn--open');
-// $('.header__info').toggleClass('header__info--open');
+});
